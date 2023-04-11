25e54f28b359e3b8a75a95d468b48fdbdbc7007a
Josh DiMatteo <joshmdimatteo.dev@gmail.com>
2023-04-11 12:09:12 -0500

    Introduces activation functions to AI file.

--- ai.py
+++ ai.py
@@ -1,3 +1,23 @@
-class AI:
-    class Neuron:
-        pass
+import numpy as np
+
+
+def linear(n):
+    return 4 * n
+
+
+def relu(n):
+    if n > 0:
+        return n
+    return 0
+
+
+def l_relu(n):
+    if n > 0:
+        return n
+    return 0.01 * n
+
+
+def p_relu(a, n):
+    if n > 0:
+        return n
+    return a * n
